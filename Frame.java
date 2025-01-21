b0398adcc927e38dc363cd478ef0eb57c155fc5f
Hariom-Jaiswal <hariomj901@gmail.com>
2025-01-22 01:58:29 +0530

    updated

---
+++ Frame.java
@@ -0,0 +1,26 @@
+
+import javax.swing.*;
+
+public class Frame{
+    
+    public static void main(String[]args){
+        JFrame frame=new JFrame("learning");
+        JLabel label=new JLabel("library management-JavaTpoint");
+        label.setBounds(40,2,200,42);
+        JButton button=new JButton("admin login");
+        button.setBounds(60,50,100,50);
+        JButton button2=new JButton("kar le login");
+        button2.setBounds(60,120,100,50);
+
+
+
+
+        frame.setLayout(null);
+        frame.add(label);
+        frame.add(button);
+        frame.add(button2);
+
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+}   
---
+++ Regi.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+
+public class Regi {
+    
+    JFrame frame = new JFrame("User Registration Form");
+    
+    public void init(){
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setSize(800, 800);
+        frame.setLocationRelativeTo(null);
+
+        JLabel title = new JLabel("Registration Form");
+        title.setBounds(30, 10, 150, 20);
+
+        JLabel name = new JLabel("Name");
+        JTextField nameT = new JTextField(20);
+        name.setBounds(30, 60, 150, 20);
+        nameT.setBounds(200, 60, 150, 20);
+
+        JLabel fatherName = new JLabel("Father Name");
+        JTextField fatherNameT = new JTextField(20);
+        fatherName.setBounds(30, 100, 150, 20);
+        fatherNameT.setBounds(200, 100, 150, 20);
+
+        JLabel age = new JLabel("Age");
+        JTextField ageT = new JTextField(20);
+        age.setBounds(30, 140, 150, 20);
+        ageT.setBounds(200, 140, 150, 20);
+
+        JLabel gender = new JLabel("Gender");
+        JRadioButton male = new JRadioButton("Male");
+        JRadioButton female = new JRadioButton("Female");
+        gender.setBounds(30, 180, 150, 20);
+        male.setBounds(200, 180, 100, 20);
+        female.setBounds(300, 180, 100, 20);
+
+        String courses[] = {"Java", "Machine Learning", "Web Tech", "EPS"};
+        JLabel course = new JLabel("Course");
+        JComboBox courseT = new JComboBox(courses);
+        course.setBounds(30, 220, 150, 20);
+        courseT.setBounds(200, 220, 150, 20);
+
+        JLabel hobbies = new JLabel("Hobbies");
+        JCheckBox drawing = new JCheckBox("Drawing");
+        JCheckBox singing = new JCheckBox("Singing");
+        JCheckBox music = new JCheckBox("Music");
+        JCheckBox others = new JCheckBox("Others");
+        hobbies.setBounds(30, 260, 150, 20);
+        drawing.setBounds(200, 260, 100, 20);
+        singing.setBounds(300, 260, 100, 20);
+        music.setBounds(400, 260, 100, 20);
+        others.setBounds(500, 260, 100, 20);
+
+        JLabel address = new JLabel("Address");
+        JTextArea addressT = new JTextArea(" Your Address....");
+        address.setBounds(30, 300, 150, 20);
+        addressT.setBounds(200, 300, 400, 60);
+
+        JButton save = new JButton("Save");
+        JButton clear = new JButton("clear");
+
+        frame.add(save);
+        frame.add(clear);
+        frame.add(address);
+        frame.add(addressT);
+        frame.add(others);
+        frame.add(music);
+        frame.add(singing);
+        frame.add(drawing);
+        frame.add(hobbies);
+        frame.add(courseT);
+        frame.add(course);
+        frame.add(female);
+        frame.add(male);
+        frame.add(gender);
+        frame.add(ageT);
+        frame.add(age);
+        frame.add(fatherNameT);
+        frame.add(fatherName);
+        frame.add(nameT);
+        frame.add(name);
+        frame.add(title);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void progressBar(){
+        JProgressBar jb;    
+        int i=0;     
+            
+        jb=new JProgressBar(0,2000);    
+        jb.setBounds(200, 400, 400, 60);         
+        jb.setValue(0);    
+        jb.setStringPainted(true);    
+        frame.add(jb);
+
+        while(i<=2000){    
+            jb.setValue(i);    
+            i=i+20;    
+            try{Thread.sleep(150);}catch(Exception e){}    
+        }    
+    }
+
+     
+
+    public static void main(String[] args) {
+        Regi call = new Regi();
+        call.init(); 
+    }
+}
